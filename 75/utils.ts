f24d8e8167cbf1efc21e4e2dda419d001176fe94
wcw <344078971@qq.com>
2024-06-06 11:29:38 +0800

    chore: add date structure of Binary Tree

--- 75/utils.ts
+++ 75/utils.ts
@@ -1,3 +1,6 @@
+/**
+ * Linked List
+ */
 export class ListNode {
     val: number
     next: ListNode | null
@@ -19,4 +22,34 @@ export function array2ListNode(array: number[]): ListNode | null {
     }
     if (head.next) head = head.next
     return head
+}
+
+/**
+ * Binary Tree
+ */
+
+export class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+export function array2TreeNode(array: number[]): TreeNode | null {
+    if (!array.length) return null
+
+    const arr = array.map(n => n !== null ? new TreeNode(n) : null)
+
+    for (let i = 0; i < arr.length; i++) {
+        const node = arr[i]
+        if (!node) continue
+        node.left = arr[2 * i + 1]
+        node.right = arr[2 * i + 2]
+    }
+
+    return arr[0]
 }
