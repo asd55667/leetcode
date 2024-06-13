c25b3ff0f2f8cd3de44e4c07da1e0cc47f9c32bb
wcw <344078971@qq.com>
2024-06-13 21:31:27 +0800

    chore: ignore children of empty node when convert array to Binary Tree

--- 75/utils.ts
+++ 75/utils.ts
@@ -44,11 +44,16 @@ export function array2TreeNode(array: (number | null)[]): TreeNode | null {
 
     const arr = array.map(n => n !== null ? new TreeNode(n) : null)
 
+    // children of empty node doesn't listed in array
+    let offset = 0
     for (let i = 0; i < arr.length; i++) {
         const node = arr[i]
-        if (!node) continue
-        node.left = arr[2 * i + 1]
-        node.right = arr[2 * i + 2]
+        if (!node) {
+            offset += 2
+            continue
+        }
+        node.left = arr[2 * i + 1 - offset] ?? null
+        node.right = arr[2 * i + 2 - offset] ?? null
     }
 
     return arr[0]
